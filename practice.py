d9c506e49307237b6f7195c2c7615e7d23e71d7e
hsumpena <46273652+hsumpena@users.noreply.github.com>
2019-01-03 19:08:08 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,8 +1,6 @@
 import random
 a = random.sample(range(97,123),10)
 b = random.sample(range(97,123),15)
-aa=[]; bb=[]
-aaa=[]; bbb=[]; ccc=[]; ddd=[]
 for x in a: aa.append(chr(x))
 for y in b: bb.append(chr(y))
 print(aa)
